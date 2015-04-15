19227191ab9664f44d46975d846ea420f8ed7238
unknown <gap8240@GDD-54.main.ad.rit.edu>
2015-04-15 19:51:20 -0400

    A board should be drawing

--- Checkers/src/CheckersGame.cpp
+++ Checkers/src/CheckersGame.cpp
@@ -12,21 +12,21 @@ class CheckersGame : public Game
 {
 public:
 	Board* board;
-	Material* mat;
+
+	Material* boardMat;
 	Shader* ballShader;
 	Camera* camera;
 	GameObject* cameraObject;
+
+	GameObject* boardObject;
+	Mesh* boardMesh;
+	MeshRenderer* boardRenderer;
+	Texture* testTexture;
 	CheckersGame::CheckersGame() : Game(800, 600)
 	{	
-		ballShader = new Shader("VertexShader.hlsl", "PixelShader.hlsl");
-		//board->addBall(new Ball()
-		camera = new Camera(50, 0.1f, 0.1f, 0.1f, 100.0f);
-		camera->setActive();
-		cameraObject = new GameObject();
-		cameraObject->addComponent(camera);
+		
 
-		for (unsigned int i = 0; i < gameObjects.size(); i++)
-			gameObjects[i]->Start();
+
 
 	}
 
@@ -36,6 +36,33 @@ public:
 
 		if (!good)
 			return false;
+
+		ballShader = new Shader("Shaders/VertexShader", "Shaders/PixelShader");
+		testTexture = new Texture("Textures/boardTextureTest.bmp");
+		//board->addBall(new Ball()
+		camera = new Camera(50, 0.1f, 0.1f, 0.1f, 100.0f);
+		camera->setLookAt(Vector3(0.0f, 0.0f, 0.0f));
+		camera->setActive();
+		cameraObject = new GameObject();
+		cameraObject->addComponent(camera);
+
+
+		boardMat = new Material(ballShader);
+
+		boardMesh = new Mesh("Models/d20Dimpled.obj");
+		boardMat->setTexture("diffuseTexture", testTexture);
+		boardRenderer = new MeshRenderer(boardMesh, boardMat);
+		boardObject = new GameObject();
+		boardObject->addComponent(boardRenderer);
+		cameraObject->getTransform()->setPosition(Vector3(0.0f, 0.0f, 5.0f));
+		boardObject->getTransform()->setPosition(Vector3(0.0f, 0.0f, 0.0f));
+
+
+
+
+		for (unsigned int i = 0; i < gameObjects.size(); i++)
+			gameObjects[i]->Start();
+
 
 		return true;
 	}
